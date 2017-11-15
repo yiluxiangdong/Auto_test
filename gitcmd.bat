4845a4cc240e4e037cfb05e6f166a5d312d6958b
unknown <liuxiaobing@xgd.com>
2017-11-15 09:07:01 +0800

    first commit

---
+++ gitcmd.bat
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "first commit"
+git  remote add origin https://github.com/yiluxiangdong/Auto_test.git
+git pull --rebase origin master
+git push -u origin master
+pause
